9326354e0f36dd39e0eaf855225162333313faef
Varsharajan <38486713+Varsharajan@users.noreply.github.com>
2025-02-26 23:34:23 +0530

    Time: 2 ms (2.66%), Space: 6.3 MB (100%) - LeetHub

--- 2427-number-of-common-factors/2427-number-of-common-factors.cpp
+++ 2427-number-of-common-factors/2427-number-of-common-factors.cpp
@@ -1,13 +1,18 @@
 class Solution {
 public:
     int commonFactors(int a, int b) {
-        int n,count =0;
-        n =min(a,b);
-        for(int i=1;i<=n;i++)
+        int count =0;
+        int min =0;
+        if(a<=b)
+        min =a;
+        else
+        min =b;
+        for(int i =1;i<=min;i++)
         {
             if(a%i==0 && b%i==0)
-               count++;
+              count++;
         }
         return count;
+        
     }
 };
