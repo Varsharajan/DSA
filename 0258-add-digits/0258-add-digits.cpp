08a628cbb65d3a4990849a2af24f1c7af8f4e97a
Varsharajan <38486713+Varsharajan@users.noreply.github.com>
2025-03-01 08:07:27 +0530

    Time: 0 ms (100%), Space: 8.7 MB (44.22%) - LeetHub

--- 0258-add-digits/0258-add-digits.cpp
+++ 0258-add-digits/0258-add-digits.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
     int addDigits(int num) {
-        if(num==0)
-        return 0;
-        else if (num%9==0)
-        return 9;
+        if(num<=9) 
+          return num;
+        else if(num%9==0)
+          return 9;
         else 
-        return (num%9);
+          return num%9;
     }
+   // TC(O(1))
 };
