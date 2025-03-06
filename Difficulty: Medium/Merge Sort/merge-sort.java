94dd58796f3a360504f91cb6f56210a4612be32f
Varsharajan <38486713+Varsharajan@users.noreply.github.com>
2025-03-06 21:23:29 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Merge Sort/merge-sort.java
+++ Difficulty: Medium/Merge Sort/merge-sort.java
@@ -38,38 +38,32 @@ class Main {
 
 
 class Solution {
-
-    void mergeSort(int arr[], int l, int r) {
-       if(l==r)
-        return;
-        int mid = (l+r)/2;
+  void mergeSort(int arr[], int l, int r) {
+        if(l>=r)
+            return;
+        int mid = l+(r-l)/2;
         mergeSort(arr,l,mid);
         mergeSort(arr,mid+1,r);
         merge(arr,l,mid,r);
-        
+           
     }
     
     void merge(int arr[],int l,int mid,int r)
     {
-        int i = l;
-        int j = mid+1;
-        int temp[] = new int[r-l+1];
-        int k=0;
+        int[] temp = new int[r-l+1];
+        int i =l, j= mid+1,k=0;
         while(i<=mid && j<=r)
         {
             if(arr[i]<=arr[j])
-            {
-               temp[k++]= arr[i++];
-            }
+              temp[k++] = arr[i++];
             else
               temp[k++] = arr[j++];
         }
         while(i<=mid)
-           temp[k++]= arr[i++];
-         while(j<=r)
+            temp[k++] = arr[i++];
+        while(j<=r)
             temp[k++] = arr[j++];
         for(i=0;i<k;i++)
-          arr[i+l] = temp[i];
+           arr[l+i] = temp[i];
     }
 }
-
