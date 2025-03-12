9f5e905ad45e720335bc5707454ce05f2663e432
Varsharajan <38486713+Varsharajan@users.noreply.github.com>
2025-03-12 20:03:19 +0530

    Time: 30 ms (67.54%), Space: 51.4 MB (84.03%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -4,8 +4,8 @@ class Solution {
         Arrays.sort(nums);
         for(int i =0;i<nums.length-2;i++)
         {
-            while(i>0 && nums[i]==nums[i-1]) 
-              i++;
+            if(i>0 && nums[i]==nums[i-1]) 
+              continue;
             int left = i+1, right= nums.length-1;
             while(left<right)
             {
