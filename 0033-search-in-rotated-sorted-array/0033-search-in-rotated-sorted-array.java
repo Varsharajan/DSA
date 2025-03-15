a5cf4661700eb1074f9241edaeb1df99d1850e20
Varsharajan <38486713+Varsharajan@users.noreply.github.com>
2025-03-15 15:06:52 +0530

    Time: 0 ms (100%), Space: 42.4 MB (28.7%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,27 +1,26 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int start = 0, end = nums.length-1;
-        while(start<=end)
-        {
-            int mid = start+(end-start)/2;
-            if(target ==nums[mid])
-               return mid;
-            if(nums[start]<=nums[mid])
+      int start =0, end= nums.length-1,result =-1;
+      while(start<=end)
+      {
+        int mid = start+(end-start)/2;
+        if(target==nums[mid])
+           return mid;
+        else if(nums[start]<=nums[mid])  // left sorted
+           {
+              if(nums[start]<=target && target<nums[mid])
+                 end = mid-1;
+               else
+                 start = mid+1;
+           }
+           else
             {
-                     if(nums[start]<= target&& target<nums[mid])
-                        end = mid-1;
-                     else
-                       start = mid+1;
+                if(nums[mid]<target && target<=nums[end])
+                   start = mid+1;
+                else
+                   end = mid-1;
             }
-            else if(nums[mid]<=nums[end])
-            {
-                    if(nums[mid]<target && target<=nums[end])
-                        start= mid+1;
-                    else
-                       end = mid-1;
-            }
-        }
-        return -1;
-
+      }
+          return -1;
     }
-}// O(logn) SC =O(1)
+}
