11d68492cdbbcab2b69351eb8b9017ecef766478
Varsharajan <38486713+Varsharajan@users.noreply.github.com>
2025-03-17 22:17:50 +0530

    Time: 24 ms (74.9%), Space: 46.8 MB (33.75%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -9,10 +9,7 @@ class Solution {
               ans++;
             if(map.containsKey(sum-k))
                ans+=map.get(sum-k);
-            if(map.containsKey(sum))
-              map.put(sum,map.get(sum)+1);
-            else
-              map.put(sum,1);
+            map.put(sum,map.getOrDefault(sum,0)+1);
         }
         return ans;
     }
