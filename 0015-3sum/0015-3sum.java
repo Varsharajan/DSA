f5a3bb21b418d0ed338ae8db7babb62272a69ea3
Varsharajan <38486713+Varsharajan@users.noreply.github.com>
2025-03-18 20:30:38 +0530

    Time: 31 ms (58.15%), Space: 51.6 MB (49.98%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -5,32 +5,32 @@ class Solution {
         int n = nums.length;
         for(int i =0;i<n-2;i++)
         {
-            if(i>0 && nums[i]==nums[i-1]) 
+            if(i>0 && nums[i]==nums[i-1])
               continue;
-            int left = i+1, right= nums.length-1;
-            while(left<right)
+            int j =i+1;
+            int k = n-1;
+            while(j<k)
             {
-                int sum = nums[i]+nums[left]+nums[right];
+                int sum = nums[i]+nums[j]+nums[k];
                 if(sum==0)
                 {
-                       result.add(Arrays.asList(nums[i],nums[left],nums[right]));
-                      
-                       while(left<right && nums[left]==nums[left+1])  
-                         left++;
-                     
-                       while(left<right && nums[right]==nums[right-1])
-                          right--;
-                       
-                        left++;
-                        right--;
+                    result.add(Arrays.asList(nums[i],nums[j],nums[k]));
+                    while(j<k && nums[j]==nums[j+1])
+                        j++;
+                    j++;
+                    while(j<k && nums[k]==nums[k-1])
+                    k--;
+                    k--;
                 }
-                else if(sum>0)
-                  right--;
+                else if(sum<0)
+                {
+                    j++;
+                }
                 else
-                 left++;
+                    k--;
             }
-        }
-        return result;
 
+        }
+      return result;
     }
 }
