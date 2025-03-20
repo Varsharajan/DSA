b915bbecacaf787530ed987be6c4bfb98f93995e
Varsharajan <38486713+Varsharajan@users.noreply.github.com>
2025-03-20 22:35:23 +0530

    Time: 0 ms (100%), Space: 46.6 MB (28.4%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,32 +1,32 @@
 class Solution {
     public int trap(int[] height) {
-        int n = height.length-1,ans=0;
-        int left =0, right = n;
-        int leftBar = height[0], rightBar = height[n];
+        int n = height.length-1;
+        int result = 0;
+        int left =0, right=n;
+        int leftBar =height[0], rightBar = height[n];
         while(left<right)
         {
-            if(leftBar < rightBar)
+            if(leftBar<rightBar)
             {
                 if(height[left]>leftBar)
-                   leftBar = height[left];
+                    leftBar= height[left];
                 else
                 {
-                  ans+=leftBar-height[left];
-                  left++;
+                   result+=leftBar-height[left];
+                   left++;
                 }
-
             }
             else
             {
                 if(height[right]>rightBar)
-                    rightBar = height[right];
+                   rightBar=height[right];
                 else
                 {
-                   ans+=rightBar-height[right];
+                   result+=rightBar-height[right];
                    right--;
                 }
             }
         }
-        return ans;
+        return result;
     }
 }
