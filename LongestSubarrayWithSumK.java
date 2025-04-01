525f2a18777d827909275b6be5e25230b546fef8
Varsharajan <38486713+Varsharajan@users.noreply.github.com>
2025-04-01 21:34:22 +0530

    Create LongestSubarrayWithSumK.java

---
+++ LongestSubarrayWithSumK.java
@@ -0,0 +1,21 @@
+public class Solution {
+    public static int longestSubarrayWithSumK(int []a, long k) {
+       int n= a.length, i=0,j=0,result=0;
+       long sum=0;
+       while(j<n)
+       {
+          sum+=a[j];
+          while(sum>k)
+          {
+            sum-=a[i];
+            i++;
+          }
+          if(sum==k)
+            result= Math.max(result, j-i+1);
+             
+          j++;
+       }
+       return result;
+
+}
+}
