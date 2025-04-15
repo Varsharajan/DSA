8c7b83025d52c7cb4ce8687b69c91ca75c2ca33f
Varsharajan <38486713+Varsharajan@users.noreply.github.com>
2025-04-15 21:52:37 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/All divisors of a Number/all-divisors-of-a-number.java
+++ Difficulty: Easy/All divisors of a Number/all-divisors-of-a-number.java
@@ -27,21 +27,26 @@ System.out.println("~");
 
 class Solution {
     public static void print_divisors(int n) {
-      List<Integer> l1 = new ArrayList<>();
-      List<Integer> l2 = new ArrayList<>();
-      for(int i =1;i*i<=n;i++)
-      {
-          if(n%i==0)
-          {
-              l1.add(i);
-              if(i!=(n/i))
-                 l2.add(n/i);
-          }
-      }
-      for(int nums:l1)
-        System.out.print(nums+" ");
-      for(int i = l2.size()-1;i>=0;i--)
-        System.out.print(l2.get(i)+" ");
-      
+        if(n==1)
+        {
+          System.out.print(1);
+          return;
+        }
+        ArrayList<Integer> result = new ArrayList<>();
+        int j =0;
+        for(int i =1;i*i<=n;i++)
+        {
+            if(n%i==0)
+             {
+                 System.out.print(i+" ");
+                 result.add(i);
+                
+             }
+        }
+        for(int k = result.size()-1;k>=0;k--)
+        {
+            if(result.get(k)!=n/result.get(k) )
+            System.out.print(n/result.get(k) +" ");
+        }
     }
 }
