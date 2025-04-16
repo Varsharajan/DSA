b4c602855a86a5f3280905cb1ba01fc94312bf7b
Varsharajan <38486713+Varsharajan@users.noreply.github.com>
2025-04-16 21:32:43 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Merge Sort/merge-sort.java
+++ Difficulty: Medium/Merge Sort/merge-sort.java
@@ -40,31 +40,34 @@ class Main {
 class Solution {
 
     void mergeSort(int arr[], int l, int r) {
-       if(l>=r)
-         return;
-        int mid = l+(r-l)/2;
-        mergeSort(arr,l,mid);
-        mergeSort(arr,mid+1,r);
-        merge(arr,l,mid,r);
+      if(l>=r)
+        return;
+      int mid = l+(r-l)/2;
+      mergeSort(arr,l,mid);
+      mergeSort(arr,mid+1,r);
+      merge(arr,l,mid,r);
+      
     }
     
-    void merge(int arr[],int l , int mid, int r)
+    void merge(int arr[],int l,int mid,int r)
     {
-        int i = l, j= mid+1,k=0;
-        int[] temp = new int[r-l+1];
+        int[] result = new int[r-l+1];
+        int i =l, j= mid+1, k=0;
         while(i<=mid && j<=r)
         {
             if(arr[i]<=arr[j])
-              temp[k++] = arr[i++];
-            else
-              temp[k++] = arr[j++];
+               result[k++] = arr[i++];
+             else
+               result[k++] = arr[j++];
         }
         while(i<=mid)
-            temp[k++] = arr[i++];
+           result[k++] = arr[i++];
         while(j<=r)
-              temp[k++] = arr[j++];
-        for(i= 0;i<k;i++)
-           arr[i+l] = temp[i];
-        
+           result[k++] = arr[j++];
+           
+        for(i=0;i<k;i++)
+        {
+            arr[l+i] = result[i];
+        }
     }
 }
