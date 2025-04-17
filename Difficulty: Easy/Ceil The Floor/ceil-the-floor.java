c9099c1200859188fb72fe783243986365aaac03
Varsharajan <38486713+Varsharajan@users.noreply.github.com>
2025-04-17 16:16:07 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Ceil The Floor/ceil-the-floor.java
+++ Difficulty: Easy/Ceil The Floor/ceil-the-floor.java
@@ -31,14 +31,17 @@ System.out.println("~");
 
 class Solution {
     public int[] getFloorAndCeil(int x, int[] arr) {
-        int floor =-1, ceil =-1;
-        for(int i =0;i<arr.length;i++)
-        {
-            if(arr[i]<=x && (arr[i]>=floor))
-               floor = arr[i];
-             if(arr[i]>=x && (ceil ==-1 || arr[i]<=ceil))
-               ceil = arr[i];
-        }
-        return new int[]{floor,ceil};
+        Arrays.sort(arr);
+        int n = arr.length;
+         int floor =-1, ceil =-1;
+         for(int i =0;i<n;i++)
+         {
+             if(arr[i]<=x && arr[i]>=floor)
+                floor = arr[i];
+             if(arr[i]>=x &&(arr[i]<=ceil || ceil ==-1))
+                ceil = arr[i];
+         }
+         return new int[]{floor,ceil};
     }
+    
 }
