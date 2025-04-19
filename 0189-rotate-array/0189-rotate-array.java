02cf68086446a3b078efe130680e95e1b3ed5c04
Varsharajan <38486713+Varsharajan@users.noreply.github.com>
2025-04-19 17:15:46 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,25 +1,21 @@
 class Solution {
     public void rotate(int[] nums, int k) {
-        int n = nums.length;
-        if( k==0 || k ==n )
-          return;
-        if(k>n)
-           k = k%n;
-        swap(nums,0,n-1); // rotate entire array
-        swap(nums,0,k-1); // rotate first k elements
-        swap(nums,k,n-1); // rotate n-k elements
-        
+        int n = nums.length-1;
+        if(n<1)
+         return;
+        rotateArray(nums,0,n);
+        rotateArray(nums,0,k-1);
+        rotateArray(nums,k,n);
     }
-
-    public void swap(int[] nums, int start, int end)
+    void rotateArray(int[] nums,int start,int end)
     {
         while(start<end)
-         {
+        {
             int temp = nums[start];
             nums[start] = nums[end];
             nums[end] = temp;
             start++;
             end--;
-         }
+        }
     }
 }
