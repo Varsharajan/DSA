610d79215421d46439566fa9efd79ffca1a0b3f7
Varsharajan <38486713+Varsharajan@users.noreply.github.com>
2025-04-28 19:31:55 +0530

    Time: 32 ms (82.33%), Space: 57.5 MB (74.23%) - LeetHub

--- 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.java
+++ 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.java
@@ -1,37 +1,28 @@
 class Solution {
     public long maximumSubarraySum(int[] nums, int k) {
-         int i = 0, j = 0, n = nums.length;
-        long sum = 0, maxSum = Long.MIN_VALUE;
-        Set<Integer> set = new HashSet<>(); // Stores unique elements
-
-        while (j < n) {
-            // Add new element to sum & set
-            sum += nums[j];
-
-            // If duplicate found, shrink from left
-            while (set.contains(nums[j]))
+        HashSet<Integer> element = new HashSet<>();
+        long sum =0, result =0;
+        int i =0,j=0,n=nums.length;
+        while(j<n)
+        {
+           
+            while(element.contains(nums[j]))
             {
-                sum -= nums[i];
-                set.remove(nums[i]);
+                element.remove(nums[i]);
+                sum-=nums[i];
                 i++;
             }
-
-            set.add(nums[j]);
-
-            // When window size reaches k
-            if (j - i + 1 == k) 
-            {
-                maxSum = Math.max(maxSum, sum);
-                
-                // Remove leftmost element to slide window
-                sum -= nums[i];
-                set.remove(nums[i]);
-                i++;
-            }
-            
-            j++; // Expand window
+           element.add(nums[j]);
+           sum+=nums[j];
+           if(j-i+1==k)
+           {
+              result = Math.max(sum,result);
+              sum-=nums[i];
+              element.remove(nums[i]);
+              i++;
+           }
+        j++;
         }
-
-        return maxSum == Long.MIN_VALUE ? 0 : maxSum; 
+        return result;
     }
 }
