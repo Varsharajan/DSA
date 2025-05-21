4cd79303977b25df4599961b8ce8eb64ae85dd37
Varsharajan <38486713+Varsharajan@users.noreply.github.com>
2025-05-21 19:44:51 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/All divisors of a Number/all-divisors-of-a-number.java
+++ Difficulty: Easy/All divisors of a Number/all-divisors-of-a-number.java
@@ -1,52 +1,19 @@
-//{ Driver Code Starts
-import java.io.*;
-import java.util.*;
-
-class GFG {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br =
-            new BufferedReader(new InputStreamReader(System.in));
-        int t;
-        t = Integer.parseInt(br.readLine());
-        while (t-- > 0) {
-
-            int n;
-            n = Integer.parseInt(br.readLine());
-
-            Solution obj = new Solution();
-            obj.print_divisors(n);
-            System.out.println();
-        
-System.out.println("~");
-}
-    }
-}
-
-// } Driver Code Ends
-
-
 class Solution {
     public static void print_divisors(int n) {
-        if(n==1)
-        {
-          System.out.print(1);
-          return;
-        }
-        ArrayList<Integer> result = new ArrayList<>();
-        int j =0;
-        for(int i =1;i*i<=n;i++)
-        {
-            if(n%i==0)
-             {
-                 System.out.print(i+" ");
-                 result.add(i);
-                
-             }
-        }
-        for(int k = result.size()-1;k>=0;k--)
-        {
-            if(result.get(k)!=n/result.get(k) )
-            System.out.print(n/result.get(k) +" ");
-        }
+       ArrayList<Integer> result = new ArrayList<>();
+       for(int i =1;i*i<=n;i++)
+       {
+           if(n%i==0)
+           {
+              result.add(i);
+              System.out.print(i+" ");
+           }
+       }
+       for(int j = result.size()-1;j>=0;j--)
+          {
+              if(n/result.get(j)!=result.get(j))
+                 System.out.print(n/result.get(j)+" ");
+          }
+        
     }
 }
